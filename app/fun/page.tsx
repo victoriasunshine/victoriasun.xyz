bf4e2870763cb28fd75537e4be8c97eb34f55be0
Victoriasunshine <vpsun@uci.edu>
2026-07-15 23:38:44 -0700

    Clean up

--- app/fun/page.tsx
+++ app/fun/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+/*import Link from "next/link";
 import { SplitContentLayout } from "@/components/layout/SplitContentLayout";
 import { client } from "@/sanity/lib/client";
 import { blogPostsQuery } from "@/sanity/lib/queries";
@@ -143,3 +143,4 @@ export default async function FunPage() {
     </SplitContentLayout>
   );
 }
+*/
